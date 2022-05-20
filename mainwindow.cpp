df8f14b898ef799103918a4595fde15f821a6a55
Treelhj <1553504297@qq.com>
2022-05-20 14:31:04 +0800

    “first_commit
    ;
    git commit -m “first_commit

---
+++ mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //播放按钮
+//    ui->PlayButton->setIcon(QIcon(":/images/play.png"));
+//    ui->PlayButton->setIconSize(QSize(30,30));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
