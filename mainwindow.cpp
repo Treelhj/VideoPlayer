eb8e4cb25e6491ac7fe96ccae31908309395ae21
Treelhj <1553504297@qq.com>
2022-05-21 22:41:43 +0800

    实现了播放列表

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,19 +1,101 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDebug>
+#include <QFileDialog>
+#include <QItemSelectionModel>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle("播放器");
 
-    //播放按钮22
-//    ui->PlayButton->setIcon(QIcon(":/images/play.png"));
-//    ui->PlayButton->setIconSize(QSize(30,30));
+    pPlayer=new QMediaPlayer;
+    myPlayList=new QStringList();
+
+    ui->listWidget->setSelectionMode(QAbstractItemView::ExtendedSelection);
+    connect(this,&MainWindow::itemRemove,ui->listWidget,[=](){
+        int count=ui->listWidget->count();
+        if (count > 0)
+        {
+           for(int i=0;i<count;i++)
+            {
+                 QString text=QString::number(i+1)+"."+ui->listWidget->item(i)->text().split('.').at(1);
+                  qDebug()<<text;
+                 ui->listWidget->item(i)->setText(text);
+            }
+        }
+    });
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+//上传视频
+void MainWindow::on_uploadBtn_clicked()
+{
+
+}
+
+
+void MainWindow::on_playBtn_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    qDebug()<<"我被点击了";
+    QStringList filenames= QFileDialog::getOpenFileNames(this,"电影列表?","C:/Users/15535/Desktop","allfiles(*.*);;""MP3(*.mp3");
+
+    foreach(QString const&str,filenames){
+        qDebug()<<str;
+        myPlayList->append(str);
+
+        QFileInfo   fileInfo(str);
+        int index=ui->listWidget->count();
+        QString text=QString::number(index+1)+"."+fileInfo.fileName();
+        ui->listWidget->addItem(text);//添加到文件列表
+
+    }
+     qDebug()<<"..................";
+ //    foreach(QString const&str,myPlayList){
+ //         qDebug()<<str;
+ //    }
+//     for(int i=0;i<myPlayList->count();i++)
+//         {
+//             QString aFile=myPlayList->at(i);
+//             qDebug()<<aFile;
+//     }
+}
+
+
+void MainWindow::on_pushButton_7_clicked()
+{
+     QList<QListWidgetItem*> items = ui->listWidget->selectedItems();
+     if (items.count() > 0)
+      {
+             foreach(QListWidgetItem* var, items)
+             {
+                  int index=ui->listWidget->row(var);
+                  myPlayList->removeAt(index);
+                  ui->listWidget->removeItemWidget(var);
+                  items.removeOne(var);
+                  delete var;
+             }
+//             for(int i=0;i<myPlayList->count();i++)
+//                 {
+//                     QString aFile=myPlayList->at(i);
+//                     qDebug()<<aFile;
+//             }
+       }
+     emit itemRemove();
+
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMediaPlayer>//播放器
+#include <QVideoFrame>//显示窗口
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +18,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+
+    void on_uploadBtn_clicked();
+
+    void on_playBtn_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+signals:
+    void itemRemove();
+
 private:
     Ui::MainWindow *ui;
+    QMediaPlayer* pPlayer;
+    QStringList* myPlayList;
 };
 #endif // MAINWINDOW_H
